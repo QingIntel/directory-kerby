7a6f559544b86684f5e291e5c81e3aa3051ca7c2
ChenSammi <sammi.chen@intel.com>
2016-10-17 14:01:28 +0800

    Draft - Start jetty to further host KDC admin web server

---
+++ kerby-kerb/kerb-admin-server/src/main/java/org/apache/kerby/kerberos/kerb/admin/server/kadmin/webserver/HelloServlet.java
@@ -0,0 +1,21 @@
+package org.apache.kerby.kerberos.kerb.admin.server.kadmin.webserver;
+
+/**
+ * Created by ychen36 on 10/16/2016.
+ */
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.getWriter().println("<h1>Hello Servlet</h1>");
+    response.getWriter().println("session=" + request.getSession(true).getId());
+  }
+}
+
